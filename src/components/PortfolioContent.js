f60a7675c591a32632b85ec5787acf26ed0d936a
James Pickering <james.pickering@gmail.com>
2022-07-24 10:02:33 +0100

    tech stack nav

--- src/components/PortfolioContent.js
+++ src/components/PortfolioContent.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function PortfolioContent({ title, desc, body }) {
+export default function PortfolioContent({ title, desc, body, techstack }) {
   return (
     <div className='content'>
       <h2 className='subtitle'>{title}</h2>
       <h3>{desc}</h3>
       <p>{body}</p>
+      <p>
+        <code>Tech stack:</code>
+      </p>
+      <ul className='tabs'>
+        {techstack.map((tech) => {
+          const tip = 'find out more about ' + tech;
+          return (
+            <li key={tech}>
+              <Link to='/about' className='tech-tab' title={tip}>
+                <code>{tech}</code>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ export default function Home({ title, headline, kidsloop, qbots, jdsports, wrenk
       <section className='hero' tabIndex='0'>
         <div className='content'>
           <h1 className='title'>{title}</h1>
-          <h2 className='headline mt-1'>{headline}</h2>
+          <h2 className='headline'>{headline}</h2>
         </div>
       </section>
       <section className='featured fadein' tabIndex='0'>
